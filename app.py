aa839cf930b9077f6a1f73cea420caab71906004
Yanbo Li <yanbo@MacBook-Pro-4.attlocal.net>
2024-08-30 21:07:22 -0700

    remmove unused import

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory, jsonify
 import os
-import time
 import threading
 from rdkit import Chem
 from rdkit.Chem import Draw
